2831c030483fee37a730ec1884946d9ffdbc46bb
Máté Donka <matedonka@apexlab.io>
2021-12-03 12:59:43 +0100

    move data provider to own package

--- app/react-admin/components/ReactAdmin.tsx
+++ app/react-admin/components/ReactAdmin.tsx
@@ -1,6 +1,6 @@
 import React from "react"
 import { Admin, Resource } from "react-admin"
-import blitzDataProvider from "../providers/data"
+import blitzDataProvider from "@theapexlab/ra-data-blitz"
 import blitzAuthProvider from "../providers/auth"
 import { PostList } from "./PostList"
 import { UserEdit } from "./UserEdit"
@@ -8,10 +8,11 @@ import { UserList } from "./UserList"
 import { PostEdit } from "./PostEdit"
 import { UserCreate } from "./UserCreate"
 import { PostCreate } from "./PostCreate"
+import { invoke } from "blitz"
 
 const ReactAdmin = () => {
   return (
-    <Admin dataProvider={blitzDataProvider()} authProvider={blitzAuthProvider}>
+    <Admin dataProvider={blitzDataProvider({ invoke })} authProvider={blitzAuthProvider}>
       <Resource name="users" list={UserList} edit={UserEdit} create={UserCreate} />
       <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} />
     </Admin>
