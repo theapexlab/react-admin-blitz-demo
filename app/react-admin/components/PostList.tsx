3261b3231c8a9b1ef370a0f184df12235f606492
Marci_Fidy <marton.fidy@codingsans.com>
2021-12-09 11:37:52 +0100

    feat: add post list filtering

--- app/react-admin/components/PostList.tsx
+++ app/react-admin/components/PostList.tsx
@@ -1,17 +1,35 @@
-import { List, Datagrid, TextField, DateField, BooleanField, ReferenceField } from "react-admin"
+import React from "react"
+import {
+  List,
+  Datagrid,
+  TextField,
+  DateField,
+  BooleanField,
+  ReferenceField,
+  ReferenceInput,
+  SelectInput,
+  TextInput,
+} from "react-admin"
+
+const postFilters = [
+  <TextInput key="search" source="q" label="Search" alwaysOn />,
+  <ReferenceInput key="1" source="userId" label="User" reference="users" allowEmpty>
+    <SelectInput optionText="name" />
+  </ReferenceInput>,
+]
 
 export const PostList = (props) => (
-  <List {...props} exporter={false}>
+  <List filters={postFilters} {...props} exporter={false}>
     <Datagrid rowClick="edit">
       <TextField source="id" />
-      <DateField source="createdAt" />
-      <DateField source="updatedAt" />
+      <ReferenceField source="userId" reference="users">
+        <TextField source="name" />
+      </ReferenceField>
       <TextField source="title" />
       <TextField source="content" />
       <BooleanField source="published" />
-      <ReferenceField source="userId" reference="users">
-        <TextField source="id" />
-      </ReferenceField>
+      <DateField source="createdAt" />
+      <DateField source="updatedAt" />
     </Datagrid>
   </List>
 )
